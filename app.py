cfbb3fae293b2027f323572359f568b3913935ad
Sourav Panda <souravpanda127@gmail.com>
2026-06-04 22:41:26 +0530

    created streamlit app layout

---
+++ app.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import pandas as pd
+from streamlit_option_menu import option_menu
+
+
+st.set_page_config(page_title="Result Analyzer", layout="wide")
+
+st.title("A comprehensive result analysis system")
+
+with st.sidebar:
+
+    st.header("📂 Upload Student Dataset")
+    uploaded_file = st.file_uploader("Upload CSV File", type=["csv"])
+
+    st.markdown("---")
+
+    selected = option_menu(
+        menu_title="📌 Menu",
+        options=[
+            "Raw Data",
+            "Student Result",
+            "Topper",
+            "Search Student",
+            "Subject Analysis",
+            "Pass / Fail",
+            "Pivot Table"
+        ],
+        icons=[
+            "table",
+            "bar-chart",
+            "trophy",
+            "search",
+            "book",
+            "check-circle",
+            "grid"
+        ],
+        menu_icon="menu-button-wide",
+        default_index=0
+    )
+
+if uploaded_file is None:
+    st.warning("Please upload a CSV file to continue.")
+    st.stop()
+
+df = pd.read_csv(uploaded_file)
+
+if selected == "Raw Data":
+    st.subheader("📊 Raw Data")
+    st.dataframe(df)
+
+
+elif selected == "Student Result":
+    total = df.groupby("Name")["Marks"].sum()
+    avg = df.groupby("Name")["Marks"].mean()
+
+    result = pd.DataFrame({
+    "Total": total,
+    "Average": avg
+    }).reset_index()
+
+    st.dataframe(
+        result,
+        use_container_width=True,
+        column_config={
+            "Name": st.column_config.TextColumn("Student Name", width="medium"),
+            "Total": st.column_config.NumberColumn("Total Marks", width="small"),
+            "Average": st.column_config.NumberColumn("Average Marks", width="small")
+        }
+    )
+
+     
+
+
+elif selected == "Topper":
+    total = df.groupby("Name")["Marks"].sum().sort_values(ascending=False)
+
+    n = st.number_input(
+        "How many toppers do you want?",
+        min_value=1,
+        max_value=len(total),
+        value=1
+    )
+
+    st.subheader(f"🏆 Top {n} Students")
+    st.dataframe(total.head(n))
+
+elif selected == "Search Student":
+    st.subheader("🔍 Search Student")
+
+    search_name = st.text_input("Enter Student Name")
+
+    if search_name:
+
+        filtered_df = df[df["Name"].str.lower() == search_name.lower()]
+
+        if not filtered_df.empty:
+            st.success(f"Showing results for {search_name}")
+            st.dataframe(filtered_df)
+
+            total = filtered_df["Marks"].sum()
+            avg = filtered_df["Marks"].mean()
+
+            st.write(f"Total Marks: {total}")
+            st.write(f"Average Marks: {avg}")
+
+        else:
+            st.error("Student not found!")
+
+elif selected == "Subject Analysis":
+    subject_avg = df.groupby("Subject")["Marks"].mean()
+
+    st.subheader("📘 SubjectWise Average")
+    st.dataframe(subject_avg)
+
+
